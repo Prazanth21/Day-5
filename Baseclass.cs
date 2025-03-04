9388ae042c15f53704d067659f5ac4cb53783414
Prazanth21 <113328363+Prazanth21@users.noreply.github.com>
2025-03-04 09:56:22 +0530

    Add files via upload

---
+++ Baseclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LibraryProject 
+{
+    public class BaseClass 
+    {
+        protected internal string message = "Hello from BaseClass (protected internal member)\n\n";
+
+        protected internal void show() 
+        {
+            Console.WriteLine("Protected Internal Method: Accessible in same assembly and derived classes of other assemblies.");
+        }
+    }
+}
---
+++ DerivedClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using LibraryProject;
+
+class DerivedClass : BaseClass
+{
+    public void AccBaseClass() 
+    {
+        Console.WriteLine(message);
+        show();
+    }
+}
+
+class Program 
+{
+    public static void Main() 
+    {
+        DerivedClass derived = new DerivedClass();
+
+        derived.AccBaseClass();
+    }
+}
+
+/*Hello from BaseClass (protected internal member)
+
+
+Protected Internal Method: Accessible in same assembly and derived classes of other assemblies.*/
